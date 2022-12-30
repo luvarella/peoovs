ec1302a769f2c0c21da9890e1dab2e833019d535
Luiza Varella Góis <luiza.varella@academico.ifrn.edu.br>
2022-12-30 16:00:40 -0300

    atualizado 30.12

---
+++ escolhinha/escolhinha/NTurma.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace escolhinha
+{
+    static class NTurma
+    {
+        // private Turma[] turmas = new Turma[10];
+        private static List<Turma> turmas = new List<Turma>();
+        public static void Inserir(Turma t)
+        { // C - Create
+            Abrir();
+            turmas.Add(t);
+            Salvar();
+        }
+        public static Turma Listar(int id)
+        {
+            foreach (Turma obj in turmas)
+                if (obj.Id == id) return obj;
+            return null;
+        }
+        public static List<Turma> Listar()
+        { // R - Read
+            return turmas;
+        }
+        public static void Atualizar(Turma t)
+        { // U - Update
+            Abrir();
+            Turma obj = Listar(t.Id);
+            obj.Curso = t.Curso;
+            obj.Descricao = t.Descricao;
+            obj.AnoLetivo = t.AnoLetivo;
+            Salvar();
+        }
+        public static void Excluir(Turma t)
+        { // D - Delete
+            Abrir();
+            turmas.Remove(Listar(t.Id));
+            Salvar();
+        }
+        public static void Abrir()
+        {
+            StreamReader f = null;
+            try
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(List<Turma>));
+                f = new StreamReader("./turma.xml");
+                turmas = (List<Turma>)xml.Deserialize(f);
+                f.Close();
+            }
+            catch
+            {
+                turmas = new List<Turma>();
+            }
+            if (f != null) f.Close();
+        }
+        public static void Salvar()
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(List<Turma>));
+            StreamWriter f = new StreamWriter("./turma.xml", false);
+            xml.Serialize(f, turmas);
+            f.Close();
+        }
+    }
+}
--- escolhinha/escolhinha/TurmaWindow.xaml.cs
+++ escolhinha/escolhinha/TurmaWindow.xaml.cs
@@ -23,5 +23,61 @@ namespace escolhinha
         {
             InitializeComponent();
         }
+
+        public void InserirClick(object sender, RoutedEventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            string curso = txtCurso.Text;
+            string desc = txtTurma.Text;
+            int ano = int.Parse(txtAno.Text);
+            Turma t = new Turma
+            {
+                Id = id,
+                Curso = curso,
+                Descricao = desc,
+                AnoLetivo = ano
+            };
+            NTurma.Inserir(t);
+            ListarClick(sender, e);
+        }
+
+        public void ListarClick(object sender, RoutedEventArgs e)
+        {
+                listTurmas.ItemsSource = null;
+                listTurmas.ItemsSource = NTurma.Listar();
+        }
+
+        public void AtualizarClick(object sender, RoutedEventArgs e)
+        {
+            obj.Curso = t.Curso;
+            obj.Descricao = t.Descricao;
+            obj.AnoLetivo = t.AnoLetivo;
+        }
+
+        private void ExcluirClick(object sender, RoutedEventArgs e)
+        {
+            if (listTurmas.SelectedItem != null)
+            {
+                NTurma.Excluir((Turma)listTurmas.SelectedItem);
+                ListarClick(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Selecione a turma a ser excluída.");
+
+            }
+        }
+
+        private void listTurmas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listTurmas.SelectedItem != null)
+            {
+                Turma obj = (Turma)listTurmas.SelectedItem;
+                txtId.Text = obj.Id.ToString();
+                txtCurso.Text = obj.Curso;
+                txtTurma.Text = obj.Descricao;
+                txtAno.Text = obj.AnoLetivo.ToString();
+            }
+        }
     }
 }
